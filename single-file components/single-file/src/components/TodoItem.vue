7f69177f83e844fb88ec98e4017d4a82f0aa1c95
tianjiax <1044880118@qq.com>
2018-04-25 21:01:31 +0800

    添加注释及 $emit传入多个参数时候的处理

--- single-file components/single-file/src/components/TodoItem.vue
+++ single-file components/single-file/src/components/TodoItem.vue
@@ -1,14 +1,20 @@
 <template>
+	<!-- 拿到父组件的传值 -->
 	<li @click="handleDelete">{{ content }}</li>  
 </template>
 
 <script>
 export default{
+	// props获取到父组件传递的方法来进行参数传递
 	props:['content','index'],
 	methods:{
+		// 删除利用 $emit 调用父级的on（@）deleteFun事件，同时传入对应的值给到父级
 		handleDelete(){
 			var self = this;
-			this.$emit('deleteFun',self.index)
+			// 传递多个参数写法
+			self.$emit('msg',{index:self.index,content:self.content})
+			// 传递单个参数写法
+			self.$emit('deleteFun',self.index)
 		}
 	}
 }
--- single-file components/single-file/src/components/TodoList.vue
+++ single-file components/single-file/src/components/TodoList.vue
@@ -3,20 +3,24 @@
 		<input v-model.trim="inputValue">
 		<button @click="handleSubmit">提交</button> 
 		<ul>
-			<todo-item v-for="(item,index) of list" @deleteFun='handleDelete' :content="item" :index="index"  :key="index"></todo-item>
+			<!-- 循环输出数组的值，并用:(v-bind) :content与:index传递内容及索引。
+			同时on（@）绑定事件让子组件可以利用 $emit 调用到该方法来执行删除或者其他操作 -->
+			<todo-item v-for="(item,index) of list" @deleteFun='handleDelete' @msg="msgFun" :content="item" :index="index"  :key="index"></todo-item>
 		</ul>
 		{{ list }}
 	</div> 
 </template>
 
 <script>
-
+// 引入子组件
 import TodoItem from './TodoItem'
 
 export default{
+	// 子组件的定义输出标签
 	components:{
 		'todo-item':TodoItem
 	},
+	// 数据存放
 	data(){
 		return {
 			inputValue:'',
@@ -24,15 +28,24 @@ export default{
 		}
 	},
 	methods:{
+		// 数组元素添加操作
 		handleSubmit(){
 			var self = this;
 			self.list.push(self.inputValue);
 			self.inputValue = '';
 		},
+		// 数组元素删除操作，获取到子组件 $emit 传入的值
 		handleDelete(index){
 			var self = this;
 			self.list.splice(index,'1');
 			self.inputValue = '';
+		},
+		// 接入$emit多个参数的时候
+		msgFun(data){
+			var self = this;
+			alert(
+				"当前删除该项的index："+data.index+"。删除该项的内容："+data.content
+			)
 		}
 	}
 }
